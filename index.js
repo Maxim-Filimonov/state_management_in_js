34d798a23dda29388cad8f305661b30db0684bed
Maxim Filimonov <tpaktopsp@gmail.com>
2017-03-04 17:18:17 +0800

    added readonly todo list

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("it works!");
+var todos = [
+  "Buy some milk",
+  "Buy some water",
+  "Clean the room"
+]
+
+function renderTodos() {
+  return todos.map(function (todo) {
+    return "<li>" + todo + "</li>";
+  });
+}
+
+$(function () {
+  $("#todos").html(renderTodos());
+})
