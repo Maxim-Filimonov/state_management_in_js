ac7dc6fa67be76d04cf002ca3974904f95263de3
Maxim Filimonov <tpaktopsp@gmail.com>
2017-03-19 13:13:29 +0800

    finished transition to state management

--- index.js
+++ index.js
@@ -2,6 +2,79 @@ var state = {
   todos: []
 };
 
+function addTodo(stateToChange, name, index) {
+  if (index === undefined) {
+    index = 0;
+  }
+
+  var todo = {
+    text: name,
+    completed: false
+  };
+
+  stateToChange.todos.splice(index, 0, todo);
+
+  return stateToChange;
+}
+
+function complete(stateToChange, index) {
+  var todo = stateToChange.todos[index];
+
+  todo.completed = !todo.completed;
+
+  return stateToChange;
+}
+
+function deleteTodo(stateToChange, index) {
+  stateToChange.todos.splice(index, 1);
+
+  return stateToChange;
+}
+
+function save() {
+  localStorage.setItem("todos", JSON.stringify(state.todos));
+}
+
+function restore() {
+  var storedTodo = localStorage.getItem("todos");
+  if (storedTodo === "undefined") {
+    storedTodo = JSON.stringify([]);
+  }
+  state.todos = JSON.parse(storedTodo);
+
+  render(state);
+}
+
+function render(state) {
+  var htmlTodo = $(renderTodos(state.todos));
+  htmlTodo.find("input:checkbox").click(toggleTodoHandler);
+  htmlTodo.find("input:button.js-delete").click(deleteTodoHandler);
+  $("#todos").html(htmlTodo);
+}
+
+$(function () {
+  restore();
+
+  $("#add-todo").click(function (e) {
+    addTodoHandler(state.todos.length)
+  });
+
+  $("#insert-todo").click(function (e) {
+    addTodoHandler(0);
+  });
+
+  $("#hide-completed").click(function (e) {
+    $(".completed").hide();
+    $("#hide-completed").hide();
+    $("#show-completed").show();
+  });
+  $("#show-completed").click(function (e) {
+    $(".completed").show();
+    $("#hide-completed").show();
+    $("#show-completed").hide();
+  });
+});
+
 function renderTodo(todo, index) {
   var checked = "";
   var labelClass = "";
@@ -18,108 +91,64 @@ function renderTodos(todos) {
   return todos.map(renderTodo).join("");
 }
 
-// create - take some db and render it on the page
-// UPDATE - state changes -- update dom tree
-/// DELETE - element remove from the state - remove the representation of that element tree
-
-// state -> user interface
-
-function deleteTodo(e) {
+function deleteTodoHandler(e) {
   var index = parseInt($(e.target).parents("li").attr("id"));
 
-  state.todos.splice(index, 1);
-
-  save();
-  render(state);
-}
-
-function toggleTodo(e) {
-  var index = parseInt($(e.target).parents("li").attr("id"));
-
-  var todo = state.todos[index];
-  todo.completed = !todo.completed;
+  deleteTodo(state, index);
 
   render(state);
   save();
 }
 
-function save() {
-  localStorage.setItem("todos", JSON.stringify(state.todos));
+function toggleTodoHandler(e) {
+  var index = parseInt($(e.target).parents("li").attr("id"));
+
+  complete(state, index);
+
+  render(state);
+  save();
 }
 
-function render(state) {
-  var htmlTodo = $(renderTodos(state.todos));
-  htmlTodo.find("input:checkbox").click(toggleTodo);
-  htmlTodo.find("input:button.js-delete").click(deleteTodo);
-  $("#todos").html(htmlTodo);
+function addTodoHandler(index) {
+  // Get the input from dom
+  var todoName = $("#add-text").val();
+
+  // Modify the state
+  addTodo(state, todoName, index);
+
+  // render
+  render(state)
+  save();
 }
 
-function restore() {
-  var storedTodo = localStorage.getItem("todos");
-  if (storedTodo === "undefined") {
-    storedTodo = JSON.stringify([]);
+// ******* TESTS ************ 
+function testComplete() {
+  var testState = { todos: [] };
+  addTodo(testState, "Buy milk")
+  addTodo(testState, "Another")
+  complete(testState, 0);
+  var completed = function (x) {
+    return x.completed;
   }
-  state.todos = JSON.parse(storedTodo);
-
-  render(state);
+  if (testState.todos.filter(completed).length === 1) {
+    console.log("TEST PASSED");
+  } else {
+    console.log("TEST FAILED", testState);
+  }
+}
+function testDelete() {
+  var testState = { todos: [] };
+  addTodo(testState, "Buy milk")
+  addTodo(testState, "Another")
+  deleteTodo(testState, 0);
+  if (testState.todos.length === 1) {
+    console.log("TEST PASSED");
+  } else {
+    console.log("TEST FAILED", testState);
+  }
 }
 
-$(function () {
-  restore();
-
-  $("#add-todo").click(function (e) {
-    // Get the input from dom
-    var newTodo = {
-      text: $("#add-text").val(),
-      completed: false
-    }
-
-    // Modify the state
-    state.todos.push(newTodo);
-
-    // render
-    render(state)
-    save();
-  });
-
-  $("#insert-todo").click(function (e) {
-    // Get the input from dom
-    var newTodo = {
-      text: $("#add-text").val(),
-      completed: false
-    }
-
-    // Modify the state
-    state.todos.splice(0, 0, newTodo);
-
-    // render
-    render(state)
-    save();
-  });
-
-  $("#hide-completed").click(function (e) {
-    $(".completed").hide();
-    $("#hide-completed").hide();
-    $("#show-completed").show();
-  });
-  $("#show-completed").click(function (e) {
-    $(".completed").show();
-    $("#hide-completed").show();
-    $("#show-completed").hide();
-  });
-});
-
-
-// function test() {
-//   addTodo("Buy milk")
-//   addTodo("Another")
-//   completed("Buy Milk")
-//   var completed = function (x) {
-//     return x.completed;
-//   }
-//   if (state.todos.filter(completed) === 1) {
-//     console.log("TEST PASSED");
-//   } else {
-//     console.log("TEST FAILED");
-//   }
-// }
+function test() {
+  testComplete();
+  testDelete();
+}
