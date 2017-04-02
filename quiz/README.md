d497dea9b9636df45dfc7b5b55c92770e2016677
Maxim Filimonov <tpaktopsp@gmail.com>
2017-04-02 11:14:51 +0800

    initial commit for quiz

---
+++ quiz/README.md
@@ -0,0 +1,18 @@
+# Quiz app with state management
+## User Stories
+*Template*
+- As a <role>
+- I want to <goal>
+- So that I can <reason>
+
+As a user
+I want to see the welcome message
+So that I know what the quiz is about
+
+As a user
+I want to start the quiz
+So that I can test my knowledge
+
+As a user
+I want to see the result of my choice
+So that I can learn from my mistakes instantly
---
+++ quiz/index.js
@@ -0,0 +1,116 @@
+var initialState = {
+  currentQuestionIndex: undefined,
+  questions: [
+    {
+      title: 'What is javascript closujre?',
+      choices: ["It's magic", 'the combination of a function and the lexical environment within which that function was declared.'],
+      correctChoiceIndex: 1
+    }
+  ],
+  welcomeMessage: 'Hi, Welcome to Get To Know JavaScript Quiz',
+}
+
+var stateFunctions = {
+  currentQuestion: function () {
+    return this.questions[this.currentQuestionIndex];
+  },
+  extend: function (state) {
+    return Object.assign(state, stateFunctions);
+  }
+}
+
+function welcome(state) {
+  return state.welcomeMessage;
+}
+
+function nextQuestion(state) {
+  // if (state.currentQuestionIndex) {
+  //   state.currentQuestionIndex++;
+  // } else {
+  state.currentQuestionIndex = 0;
+  // }
+  return state;
+}
+
+function start(state) {
+  return nextQuestion(state);
+}
+function select(state, choiceIndex) {
+  return state.currentQuestion().correctChoiceIndex === choiceIndex;
+}
+
+/**** Tests *****/
+function testStartWithInitial() {
+  var testState = {};
+  var changedState = start(testState);
+  var actual = changedState.currentQuestionIndex;
+  if (actual === 0) {
+    console.log("PASS");
+  } else {
+    console.error("FAIL");
+  }
+}
+function stateFunctionsTests() {
+  var testState = { questions: ['42?'], currentQuestionIndex: 0 };
+  var changedState = stateFunctions.extend(testState);
+  var actual = changedState.currentQuestion();
+
+  if (actual === "42?") {
+    console.log("PASS");
+  } else {
+    console.error("FAIL");
+  }
+
+  testState.currentQuestionIndex = 1;
+  testState.questions = ['42?', 'Why?'];
+
+  var changedState = stateFunctions.extend(testState);
+  var actual = changedState.currentQuestion();
+
+  if (actual === "Why?") {
+    console.log("PASS");
+  } else {
+    console.error("FAIL");
+  }
+}
+function testStartAgain() { }
+
+function testSelect() {
+  var testState = {
+    currentQuestionIndex: 0,
+    questions: [
+      {
+        title: '42?',
+        choices: [42, 2, 3, 4],
+        correctChoiceIndex: 0
+      }
+    ]
+  };
+  stateFunctions.extend(testState);
+
+  var result = select(testState, 0);
+  if (result === true) {
+    console.log("PASS");
+  } else {
+    console.error("FAIL");
+  }
+
+  var result = select(testState, 1);
+  if (result === false) {
+    console.log("PASS");
+  } else {
+    console.error("FAIL");
+  }
+}
+
+function test() {
+  testStartWithInitial();
+  stateFunctionsTests();
+  testSelect();
+}
+
+// ** HACKY PART **
+var state = stateFunctions.extend(initialState);
+console.log(welcome(state));
+start(state);
+console.log(state.currentQuestion());
