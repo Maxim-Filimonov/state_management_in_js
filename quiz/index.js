a0e52c55a854ef3dae8a7f6e4117fa223dfe0292
Maxim Filimonov <tpaktopsp@gmail.com>
2017-04-09 11:36:02 +0800

    Adding welcome message rendering

--- quiz/index.js
+++ quiz/index.js
@@ -1,6 +1,16 @@
 
+function renderQuestion(question) {
+  return "<li>" + question.title + "</li>"
+}
+function renderQuestions(questions) {
+  return questions.map(renderQuestion);
+}
+function render() {
+  $("#welcome").html(welcome(state))
+  // $(".questions").html(renderQuestions(state.questions));
+}
 // ** HACKY PART **
 var state = stateFunctions.extend(initialState);
-console.log(welcome(state));
-start(state);
-console.log(state.currentQuestion());
+render();
+// start(state);
+// console.log(state.currentQuestion());
